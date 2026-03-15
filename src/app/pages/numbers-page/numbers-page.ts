a77e0ea33745764e07bf6fa8de1519f4ec4dd928
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-15 13:33:38 -0600

    feat: add main numeric pipes

--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html',
 })
 export default class NumbersPage {
+  totalSells = signal(2_422_232.5567)
+  percent = signal(0.5846)
 
 }
