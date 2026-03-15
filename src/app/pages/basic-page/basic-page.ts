d264210ce52d14d798c6ab9a4f723e6ce402d352
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-15 13:45:28 -0600

    feat: add basic date pipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,13 +1,15 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
 })
 export default class BasicPage {
   nameLower = signal('carlos');
   nameUpper = signal('CARLOS');
   fullName = signal('cArlOS gALINdo');
+
+  customDate = signal(new Date());
 }
