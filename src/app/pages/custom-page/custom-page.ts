cfec7bfcdbe283b93ff2948cf4c7299ef816c042
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-16 00:39:18 -0600

    feat: create pipes for transforming table data

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,15 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../mock/hero.data';
+import { CanFlyPipe } from './pipes/can-fly-pipe';
+import { ColorPipe } from './pipes/color-pipe';
+import { TextColorPipe } from './pipes/text-color-pipe';
 
 @Component({
   selector: 'custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, ColorPipe, TextColorPipe],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage {
   case = signal<'lower' | 'upper'>('lower');
 
-  name = 'Carlos Galindo'
+  name = 'Carlos Galindo';
+
+  heroes = signal([...heroes]);
 
   changeCase() {
     if(this.case() === 'lower') {
---
+++ src/app/pages/custom-page/pipes/text-color-pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Color, ColorMap } from '../../../interfaces/hero.interface';
+
+@Pipe({
+  name: 'textColor',
+})
+export class TextColorPipe implements PipeTransform {
+
+  transform(value: Color): string {
+    return ColorMap[value];
+  }
+
+}
