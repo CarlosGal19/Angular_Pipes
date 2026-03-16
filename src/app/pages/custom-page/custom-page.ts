d8e621197edd2922e43492e34a68d9005e20aa2e
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-16 00:44:36 -0600

    feat: create custom pipe for creator

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -4,10 +4,11 @@ import { heroes } from '../../mock/hero.data';
 import { CanFlyPipe } from './pipes/can-fly-pipe';
 import { ColorPipe } from './pipes/color-pipe';
 import { TextColorPipe } from './pipes/text-color-pipe';
+import { CreatorPipe } from './pipes/creator-pipe';
 
 @Component({
   selector: 'custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, ColorPipe, TextColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, ColorPipe, TextColorPipe, CreatorPipe],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage {
